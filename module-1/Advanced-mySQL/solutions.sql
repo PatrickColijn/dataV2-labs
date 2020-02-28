c779b29bf6db29540a901473d80bd6e5e3a5e407
PatrickColijn <pcolijn@deloitte.nl>
2020-02-28 10:19:27 +0100

    finished SQL solutions

--- module-1/Advanced-mySQL/solutions.sql
+++ module-1/Advanced-mySQL/solutions.sql
@@ -1,10 +1,10 @@
 '''Challenge 1 - Step 1''';
 
 SELECT 
-	authors.au_id AS author_id, 
-	titles.title AS title,
-	(titles.advance * titleauthor.royaltyper / 100) AS advance,
-	(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+    authors.au_id AS author_id, 
+    titles.title AS title,
+    (titles.advance * titleauthor.royaltyper / 100) AS advance,
+    (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
 FROM authors
 INNER JOIN titleauthor
 ON authors.au_id = titleauthor.au_id
@@ -16,41 +16,107 @@ ON titleauthor.title_id = sales.title_id;
 '''Challenge 1 - Step 2''';
 
 SELECT author_id, title, (advance + royalty) AS aggregated
-	FROM (SELECT 
-	authors.au_id AS author_id, 
-	titles.title AS title,
-	(titles.advance * titleauthor.royaltyper / 100) AS advance,
-	(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
-	FROM authors
-	INNER JOIN titleauthor
-	ON authors.au_id = titleauthor.au_id
-	INNER JOIN titles
-	ON titleauthor.title_id = titles.title_id
-	INNER JOIN sales
-	ON titleauthor.title_id = sales.title_id) summary
+    FROM (SELECT 
+    authors.au_id AS author_id, 
+    titles.title AS title,
+    (titles.advance * titleauthor.royaltyper / 100) AS advance,
+    (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+    FROM authors
+    INNER JOIN titleauthor
+    ON authors.au_id = titleauthor.au_id
+    INNER JOIN titles
+    ON titleauthor.title_id = titles.title_id
+    INNER JOIN sales
+    ON titleauthor.title_id = sales.title_id) summary
 GROUP BY author_id, title, advance, royalty;
-	
+    
 '''Challenge 1 - Step 3''';
 
 SELECT author_id, SUM(aggregated) AS total_profit
 FROM(SELECT author_id, title, (advance + royalty) AS aggregated
-		FROM (SELECT 
-		authors.au_id AS author_id, 
-		titles.title AS title,
-		(titles.advance * titleauthor.royaltyper / 100) AS advance,
-		(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
-		FROM authors
-		INNER JOIN titleauthor
-		ON authors.au_id = titleauthor.au_id
-		INNER JOIN titles
-		ON titleauthor.title_id = titles.title_id
-		INNER JOIN sales
-		ON titleauthor.title_id = sales.title_id) summary
-	GROUP BY author_id, title, advance, royalty) summary2
+        FROM (SELECT 
+        authors.au_id AS author_id, 
+        titles.title AS title,
+        (titles.advance * titleauthor.royaltyper / 100) AS advance,
+        (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+        FROM authors
+        INNER JOIN titleauthor
+        ON authors.au_id = titleauthor.au_id
+        INNER JOIN titles
+        ON titleauthor.title_id = titles.title_id
+        INNER JOIN sales
+        ON titleauthor.title_id = sales.title_id) summary
+    GROUP BY author_id, title, advance, royalty) summary2
 GROUP BY author_id
 ORDER BY total_profit DESC;
 
-'''Challenge 2'''
+'''Challenge 2 - Step 1''';
 
-# Still working on these...
-	
+CREATE TEMPORARY TABLE publications.royalty_advance
+SELECT authors.au_id AS author_id, titles.title as title, (titles.advance * titleauthor.royaltyper / 100) AS advance, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+FROM publications.authors authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id;
+
+SELECT * FROM publications.royalty_advance;
+
+'''Challenge 2 - Step 2''';
+
+CREATE TEMPORARY TABLE publications.aggregated
+SELECT author_id, title, (advance + royalty) AS aggregated
+    FROM (SELECT authors.au_id AS author_id, titles.title AS title, (titles.advance * titleauthor.royaltyper / 100) AS advance, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+FROM publications.authors authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id) summary
+GROUP BY author_id, title, advance, royalty;
+
+SELECT * FROM publications.aggregated;
+
+'''Challenge 2 - Step 3''';
+
+CREATE TEMPORARY TABLE total_profit
+SELECT author_id, SUM(aggregated) AS total_profit
+FROM(SELECT author_id, title, (advance + royalty) AS aggregated
+FROM (SELECT authors.au_id AS author_id, titles.title AS title,
+(titles.advance * titleauthor.royaltyper / 100) AS advance,
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+FROM publications.authors authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id) summary
+GROUP BY author_id, title, advance, royalty) summary2
+GROUP BY author_id
+ORDER BY total_profit DESC;
+
+SELECT * FROM publications.total_profit;
+
+'''Challenge 3''';
+
+CREATE TABLE most_profiting_authors
+SELECT author_id, SUM(aggregated) AS total_profit
+FROM(SELECT author_id, title, (advance + royalty) AS aggregated
+FROM (SELECT authors.au_id AS author_id, titles.title AS title,
+(titles.advance * titleauthor.royaltyper / 100) AS advance,
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS royalty
+FROM publications.authors authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id) summary
+GROUP BY author_id, title, advance, royalty) summary2
+GROUP BY author_id
+ORDER BY total_profit DESC;
+    
